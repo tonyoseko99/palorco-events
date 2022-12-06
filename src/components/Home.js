8d50b7feccf77983b54c11c63d11ff55c080088d
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-07 00:31:37 +0300

    feat: add a background image to the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { Col, Row } from 'antd';
+import React from "react";
+import { Col, Row } from "antd";
 const Home = () => (
   <>
     <Row>
-      <Col span={24}>col</Col>
+      <Col span={24}>
+        <img
+          src="https://images.unsplash.com/photo-1583338917496-7ea264c374ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="palorco"
+          style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+        />
+      </Col>
     </Row>
   </>
 );
