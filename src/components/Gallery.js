ffff80e0f7e43adc8926d9610c26828a32b27adc
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-06 18:59:05 +0300

    feat: Create a navigation bar

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Button } from 'antd';
 function Gallery() {
   return (
     <div>
       <h1>Gallery</h1>
+        <Button type="primary">Button</Button>
     </div>
   );
 }
